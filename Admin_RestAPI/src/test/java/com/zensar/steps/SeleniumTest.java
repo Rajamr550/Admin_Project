a1d46dfa01d94286208c050a0f6d0fb6a5110588
Prashant Dhoke <prashant.dhoke@zensar.com>
2022-08-23 15:56:16 +0530

    Add a Admin Record and Delete a Admin record Cucumber Test

--- Admin_RestAPI/src/test/java/com/zensar/steps/SeleniumTest.java
+++ Admin_RestAPI/src/test/java/com/zensar/steps/SeleniumTest.java
@@ -3,6 +3,7 @@ package com.zensar.steps;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,39 +18,26 @@ public class SeleniumTest {
 
 	@Given("open the chrome browser")
 	public void open_the_chrome_browser() {
-
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\RM66237\\Downloads\\chromedriver_win32\\chromedriver.exe");  
-	       
-        
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");    
 	     driver=new ChromeDriver();  
-	       driver.manage().window().maximize();
-
-	       
-	       
-	       
+	     driver.manage().window().setSize(new Dimension(1920, 1000));             
 	}
 
 	@When("Enter the url {string}")
 	public void enter_the_url(String url) {
-		
 		driver.get(url);
-		
 	}
 
 	@When("Click on delete button of id")
-	public void click_on_delete_button_of_id() {
-		
-		WebElement deleteButton = driver.findElement(By.xpath("//*[@id=\"DataTables_Table_0\"]/tbody/tr[1]/td[6]/button/mat-icon"));
-		deleteButton.click();
+	public void click_on_delete_button_of_id() {	
+		driver.findElement(By.name("delete")).findElement(By.id("82")).click();
 	}
-
-
 
 	@Then("Deleting works fine")
 	public void deleting_works_fine() {
 	  System.out.println("deleteByID - working fine");
-	  driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-	  driver.close();
+//	  driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+//	  driver.close();
 	}
 
 
