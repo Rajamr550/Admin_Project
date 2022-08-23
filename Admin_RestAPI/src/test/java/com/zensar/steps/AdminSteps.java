dc2f662a66079348bcfa935c319d8fa3ffe8d050
Prashant Dhoke <prashant.dhoke@zensar.com>
2022-08-23 14:29:56 +0530

    Add a Admin Record Cucumber-Selenium Test

--- Admin_RestAPI/src/test/java/com/zensar/steps/AdminSteps.java
+++ Admin_RestAPI/src/test/java/com/zensar/steps/AdminSteps.java
@@ -3,7 +3,12 @@ package com.zensar.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import junit.framework.Assert;
+
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,10 +29,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AdminSteps {
 	
+	public WebDriver  driver=null;
 	
-	
-private void Sysout() {
-	// TODO Auto-generated method stub
+	@Given("open browser")
+	public void open_browser() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\PD66188\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		driver =new ChromeDriver();
+		driver.manage().window().maximize();  
+	}
 
-}
+	@When("Enter url {string}")
+	public void enter_url(String url) {
+		driver.get(url);  
+	}
+
+	@When("Enter Admin Details like name {string} passowrd {string} email {string}")
+	public void enter_admin_details_like_name_passowrd_email(String name, String password, String email) {
+		driver.findElement(By.id("name")).sendKeys(name);
+		driver.findElement(By.id("password")).sendKeys(password); 
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.id("mobileNo")).sendKeys("878787878"); 
+	}
+
+	@When("Click on Submit")
+	public void click_on_submit() {
+		driver.findElement(By.name("submit")).click();  
+	}
+
+	@Then("Admin details get registered")
+	public void admin_details_get_registered() {
+		String capText=	driver.findElement(By.xpath("//*")).getText();
+		Assert.assertEquals(true, capText.contains("Dhoke123@gmail.com"));	   
+	}
+
+
+	
+
+//	@Given("open browser")
+//	public void open_browser() {
+//		
+//		System.setProperty("webdriver.chrome.driver","C:\\Users\\PD66188\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+//		driver =new ChromeDriver();
+//		driver.manage().window().maximize();
+//	    
+//	}
+//
+//
+//	@When("Enter url {string}")
+//	public void enter_url(String url) {
+//	   driver.get(url);
+//	}
+//
+//
+//
+//	@When("Click on delete icon to delete any admin")
+//	public void click_on_delete_icon_to_delete_any_admin() {
+//		driver.findElement(By.id("deleteRecord")).click();
+//	}
+//
+//
+//
+//	@Then("selected admin record will be deleted")
+//	public void selected_admin_record_will_be_deleted() {
+//		
+//		String capText=	driver.findElement(By.xpath("//*")).getText();
+//		Assert.assertEquals(true, capText.contains("Dhoke123@gmail.com"));
+//	   
+//	}
+
 }
--- Admin_RestAPI/src/test/resources/AdminFeature.feature
+++ Admin_RestAPI/src/test/resources/AdminFeature.feature
@@ -1,4 +1,10 @@
 Feature: Cucumber Selenium Test.
+ Scenario: Add a Admin record
+  Given open browser
+  When Enter url "http://localhost:4200/admin_form"
+  And Enter Admin Details like name "Dhoke" passowrd "Dhoke@123" email "Dhoke123@gmail.com" 
+  And Click on Submit
+  Then Admin details get registered  
 
 
    
